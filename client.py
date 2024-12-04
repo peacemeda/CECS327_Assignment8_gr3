a228c3bc23fff10647e50464e7d064391299d975
wisedev73 <wisedev73@gmail.com>
2024-12-03 20:59:02 -0800

    Update client.py

--- client.py
+++ client.py
@@ -13,17 +13,20 @@ def send_query(query, HOST, PORT):
 if __name__ == "__main__":
     HOST = input("Enter the server ip address:") # Server ip address
     PORT = int(input("Enter the server port number:")) # Server Port
-    queries = [
-        "What is the average moisture inside my kitchen fridge in the past three hours?",
-        "What is the average water consumption per cycle in my smart dishwasher?",
-        "Which device consumed more electricity among my three IoT devices?"
-    ]
-    print("Choose a query:")
-    for i, q in enumerate(queries, 1):
-        print(f"{i}. {q}")
-    choice = int(input("Enter the number of your query: "))
-    if 1 <= choice <= 3:
-        send_query(queries[choice - 1], HOST, PORT)
-    else:
-        print("Invalid query.")
-
+    choice = 1
+    while choice != 0:
+        queries = [
+            "What is the average moisture inside my kitchen fridge in the past three hours?",
+            "What is the average water consumption per cycle in my smart dishwasher?",
+            "Which device consumed more electricity among my three IoT devices?"
+        ]
+        print("Choose a query:")
+        for i, q in enumerate(queries, 1):
+            print(f"{i}. {q}")
+        choice = int(input("Enter the number of your query: "))
+        if 1 <= choice <= 3:
+            send_query(queries[choice - 1], HOST, PORT)
+        elif choice == 0:
+            break
+        else:
+            print("Invalid query.")
